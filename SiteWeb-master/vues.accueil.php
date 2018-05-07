92a23b3527105d23046ebffa3552b5be4f828e21
Yann-lyH <32388906+Yann-lyH@users.noreply.github.com>
2018-05-07 11:40:57 +0200

    Update vues.accueil.php

--- SiteWeb-master/vues.accueil.php
+++ SiteWeb-master/vues.accueil.php
@@ -1,7 +1,62 @@
-<?php 
-echo "Inscription"
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="vues.accueil.css" />
+        <title>Accueil du compte</title>
+    </head>
 
-<p>S'inscrire : <a href="index.php?cible=utilisateurs&fonction=inscription">Lien</a></p>
-<p>Liste des utilisateurs : <a href="index.php?cible=utilisateurs&fonction=liste">lien</a></p>
-<p>Liste des Capteurs : <a href="index.php?cible=capteurs">lien</a></p>
+    <body>
+    	<header>
+    		<nav>
+    			<?php include(pageHeader.php); ?>
+
+            </nav>
+ 
+
+        </header>
+
+
+
+        <section>
+        	<div id="conteneur0">
+        	<h1> Accueil du compte </h1>
+        	<p>Bienvenue madame/monsieur <?php echo "... ";?>!</p>
+        	</div>
+
+        	
+        	<div id= "conteneur1">
+        	      <div id="conteneur2">
+        			<input id="boutonDonnees" type="image" src="images site APP\cloud.jpg" value="Données"id="fig1"/>
+        			<p id="legendeDonnees">Données</p>
+        			</div>
+        			
+        			<div id="conteneur3">
+        			<input id="boutonLogement" type="image" src="images site APP\maison.png" value="Logement"id="fig2"/>
+        			<p id="legendeLogement">Logement</p>
+        			</div>
+        			
+        			<div id="conteneur4">
+        			<input id="boutonStat" type="image" src="images site APP\stat.jpg" value="Stat"id="fig4"/>
+        			<p id="legendeStat">Statistiques</p>
+        			</div>
+        			
+        			<div id="conteneur5">
+        			<input id="boutonCapteurs" type="image" src="images site APP\capteur.jpg" value="Capteurs"id="fig5"/>
+        			<p id="legendeCapteurs">Capteurs</p>
+        			
+        	</div>
+        			
+
+        		
+        	</div>
+        	
+
+
+
+        </section>
+
+    
+    </body>
+</html>
+
